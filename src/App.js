aa7c7ccd7abf27f5162b5e6f89639fae1aab0bf6
veriano <veriano@ukr.net>
2022-02-08 11:49:16 +0200

    finishing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-  import { useState, useEffect } from 'react';
-  // import PropTypes from 'prop-types';
+  import { useState } from 'react';
   import FeedbackOptions from './Components/FeedbackOptions';
   import Statistics from './Components/Statistics'; 
   import Section from './Components/Section';
@@ -9,27 +8,40 @@
     const [good, setGood] = useState(0);
     const [neutral, setNeutral] = useState(0);
     const [bad, setBad] = useState(0);
-    const [total, setTotal] = useState(0);
-    const [percentage, setPercentage] = useState(0);
-    const [option, setOption] = useState(0);
    
     const countTotalFeedback = () => {
-    useEffect(() => {
-      setTotal(good + neutral + bad);
-    },[good, neutral, bad])
+      const total = (good + neutral + bad);
+      return total;
     }
 
     const countPositiveFeedbackPercentage = () => {
-      setPercentage(100 / total * good);
+      const percentage = (100 / countTotalFeedback()) * good;
+      return percentage;
     }
    
     const handleIncrement = (option) => {
-      setOption(prevState => { prevState[option] + 1 });
+
+      switch (option) {
+        case 'good':
+          setGood(good + 1 );
+          break;
+
+        case 'neutral':
+          setNeutral(neutral + 1 );
+          break;
+
+        case 'bad':
+          setBad(bad + 1 );
+          break;
+
+        default:
+          return;
+      }
     }
 
     return (
       <div>
-        <Section title="Please leave feedback"></Section>
+        <Section title="Please leave feedback">
 
         <FeedbackOptions 
           options={ ['good', 'neutral', 'bad'] } 
@@ -44,7 +56,7 @@
           positivePercentage={ countPositiveFeedbackPercentage() }
         />
 
-        <Section />
+        </Section>
       </div>
     )
 }
